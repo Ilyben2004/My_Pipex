2645d53eb2b87766117e6a723c25d362875a0988
Ilyas Bennajeh <ibennaje@c3r5p7.1337.ma>
2025-01-13 12:51:51 +0100

    i should wait

--- pipex.c
+++ pipex.c
@@ -8,8 +8,6 @@ static void	pipex_init(pipex_t *pipex_vars, char *argv[], char *envp[])
 	i = 0;
 	pipex_vars->command1 = ft_split(argv[2], ' ');
 	pipex_vars->command2 = ft_split(argv[3], ' ');
-	
-	pipex_vars->command2[0] = ft_check_command(&(pipex_vars->command2[0]), envp);
 	pipex_vars->fd = open(argv[1], O_RDONLY);
 	if (pipex_vars->fd == -1)
 	{
@@ -20,7 +18,7 @@ static void	pipex_init(pipex_t *pipex_vars, char *argv[], char *envp[])
 	}
 	else
 		pipex_vars->command1[0] = ft_check_command(&(pipex_vars->command1[0]), envp);
-
+	pipex_vars->command2[0] = ft_check_command(&(pipex_vars->command2[0]), envp);
 	pipex_vars->fd2 = open(argv[4], O_WRONLY | O_TRUNC | O_CREAT, 0644);
 	if (pipex_vars->fd2 == -1)
 	{
@@ -31,6 +29,7 @@ static void	pipex_init(pipex_t *pipex_vars, char *argv[], char *envp[])
 		pipex_vars->command2[0] = NULL;
 	}
 	pipe(pipex_vars->pipefd);
+	
 }
 
 int	main(int argc, char *argv[], char *envp[])
@@ -46,23 +45,15 @@ int	main(int argc, char *argv[], char *envp[])
 			return (0);
 		pipex_init(pipex_vars, argv, envp);
 		pid = fork();
-		if (pid == 0 && pipex_vars->command1[0])
-		{
-			printf("i stopeed here first child");
+		if (pid == 0)
 			first_child(pipex_vars->pipefd, pipex_vars->command1,
 				pipex_vars->fd);
-		}
 		else if (pid > 0)
 		{
-			if (!pipex_vars->command2[0])
-				return (0);
 			pid2 = fork();
-			if (pid2 == 0 && pipex_vars->command2[0])
-			{
-				printf("i stopped here second child");
+			if (pid2 == 0)
 				second_child(pipex_vars->pipefd, pipex_vars->command2,
 					pipex_vars->fd2);
-			}
 		}
 		free_splited(pipex_vars->command1, pipex_vars->command2);
 		free(pipex_vars);
--- pipex_utils2.c
+++ pipex_utils2.c
@@ -48,20 +48,27 @@ int	is_path(char *command)
 
 void first_child (int * pipefd, char ** command1, int fd)
 {
-    close(pipefd[0]);
-    dup2(fd,0);
-    dup2(pipefd[1],1);
-    execve(command1[0], command1 , NULL);
-    perror("");
+    
+    if (command1[0])
+    {
+        close(pipefd[0]);
+        dup2(fd,0);
+        dup2(pipefd[1],1);
+        execve(command1[0], command1 , NULL);
+        perror("");
+    }
     exit (0);
 }
 
 void	second_child(int *pipefd, char **command2, int fd2)
 {
-	close(pipefd[1]);
+    if (command2[0])
+    {
+    close(pipefd[1]);
 	dup2(fd2, 1);
 	dup2(pipefd[0], 0);
 	execve(command2[0], command2, NULL);
 	perror("");
+    }
 	exit(0);
 }
